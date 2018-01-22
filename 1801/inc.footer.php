fa2a67b1d41cbe85ad5c227c4044b1d80ecc30f7
DESKTOP-NVGG4E7\ramon <ramon.vizmonte@openport.com>
2018-01-22 23:17:05 +0800

    moved to 1801

---
+++ 1801/inc.footer.php
@@ -0,0 +1,38 @@
+
+<section id="footer" class="row dk-green text-md-left">
+<div class="container">
+<div class="row">
+
+		<div class="col-md-4" >
+		<p>
+		<span class="text-nowrap">&copy; 2017-<?php echo $curr_year = date('y');?> <strong>MFT Group.</strong> </span>
+		<span class="text-nowrap">All Rights Reserved.</span>
+		</p>
+		</div>
+		
+		<div class="col-md-4" >
+		<p><a href="https://goo.gl/maps/GtiMEEGQtPB2" target="_blank">
+		<span class="text-nowrap">Trade And Financial Tower, </span>
+		<span class="text-nowrap">7Th Avenue, </span>
+		<span class="text-nowrap">Taguig City, </span>
+		<span class="text-nowrap">Metro Manila,</span>
+		<span class="text-nowrap">Republic of the Philippines</span>
+		</a></p>
+		</div>
+		
+		<div class="col-md-4" >
+		<p><a href="tel:+63(2)8870701">+63(2)8870701</a></p>
+		</div>
+		
+</div>
+</div>
+</section>
+
+<!--CONTAINER END-->
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
+
+
+</body>
+</html>
